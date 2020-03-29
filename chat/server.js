d6db023c0c4f8751fd9c9d5b76c61bd003c48cb4
foshanxiaoyu <1422054416@qq.com>
2020-03-29 23:49:41 +0800

    dele dir chat

--- chat/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-app.get('/',async(req,res)=>{
-    res.send('star ......')
-})
-app.listen(3001,()=>{
-    console.log("sever is listen at 3001")
-}
-)
